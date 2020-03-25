91bde933a81e1c8721be7e574568a864f0b5bd68
filip4428 <filip.strelec@speck.agency>
2020-03-25 21:15:08 +0100

    In dev

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require ('fs');
+const http = require('http');
+const request = require('request');
+
+let json= "";
+
+const server = http.createServer ((req,res) => {
+
+console.log("access!");
+
+ request('http://192.168.0.15', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+
+   json=body;
+
+
+
+});
+
+
+
+res.writeHead(200,{"Content-Type": "application/json"})
+res.writeHead(200,{"Access-Control-Allow-Origin": "*"})
+res.end(JSON.stringify(json));
+
+
+});
+
+server.listen(1337, '127.0.0.1', ()=>{
+
+console.log("listening");
+
+})
+
+
