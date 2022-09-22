d06998e4cb22296cae391dbb84b5bd466d364e4d
Filip Strelec <filip.strelec@gmail.com>
2022-09-22 16:22:15 +0200

    Add wake on Lan and other stuff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require ('fs');
 const http = require('http');
 const request = require('request');
+const wol = require('wol');
 
 let json= "";
 
@@ -11,7 +12,7 @@ const server = http.createServer ((req,res) => {
    if (req.url.includes("heat")){
 
 
-      request('http://192.168.0.15/heat', { json: true }, (err, resReq, body) => {
+      request('http://192.168.1.65/heat', { json: true }, (err, resReq, body) => {
          if (err) { return console.log(err); }
        
           json=resReq.body;
@@ -31,7 +32,7 @@ res.end(JSON.stringify(json));
 
    else if (req.url.includes("led1")){
 
-      request('http://192.168.0.15/led1', { json: true }, (err, resReq, body) => {
+      request('http://192.168.1.65/led1', { json: true }, (err, resReq, body) => {
          if (err) { return console.log(err); }
        
           json=resReq.body;
@@ -50,7 +51,7 @@ res.end(JSON.stringify(json));
 
    else if (req.url.includes("led2")){
 
-      request('http://192.168.0.15/led2', { json: true }, (err, resReq, body) => {
+      request('http://192.168.1.65/led2', { json: true }, (err, resReq, body) => {
          if (err) { return console.log(err); }
        
           json=resReq.body;
@@ -69,7 +70,7 @@ res.end(JSON.stringify(json));
 
    else if (req.url.includes("led3")){
 
-      request('http://192.168.0.15/led3', { json: true }, (err, resReq, body) => {
+      request('http://192.168.1.65/led3', { json: true }, (err, resReq, body) => {
          if (err) { return console.log(err); }
        
           json=resReq.body;
@@ -86,9 +87,9 @@ res.end(JSON.stringify(json));
 
    }
 
-   else if (req.url.includes("turnOff")){
+   else if (req.url.includes("turnoff")){
 
-      request('http://192.168.0.15/turnOff', { json: true }, (err, resReq, body) => {
+      request('http://192.168.1.65/turnOff', { json: true }, (err, resReq, body) => {
          if (err) { return console.log(err); }
        
           json=resReq.body;
@@ -105,22 +106,57 @@ res.end(JSON.stringify(json));
 
    }
 
+
+   else if (req.url.includes("wake")){
+console.log("pali pilu")
+
+wol.wake('70:85:C2:A9:xx:xx', function(err, res){
+ }).then((el)=>{
+
+console.log("FILIP", el)
+res.writeHead(200,{"Content-Type": "application/json"})
+res.writeHead(200,{"Access-Control-Allow-Origin": "*"})
+
+el ? res.end("TURNED ON"): res.end("FAILED")
+
+ });
+
+
+
+
+   }
 
 
    else{
-      request ('http://192.168.0.15/', { json: true }, (err, resReq, body) => {
-         if (err) { return console.log(err); }
-       
-          json=resReq.body;
-         //  console.log(res.body);
-         
+
+if(json ===""){
+
+   request ('http://192.168.1.65/', { json: true }, (err, resReq, body) => {
+      if (err) { return console.log(err); }
+    
+       json=resReq.body;
+      //  console.log(res.body);
+      console.log("first time access");
 
 res.writeHead(200,{"Content-Type": "application/json"})
 res.writeHead(200,{"Access-Control-Allow-Origin": "*"})
 
 res.end(JSON.stringify(json));
 
-       });
+    });
+
+
+}
+
+else {
+
+   res.writeHead(200,{"Content-Type": "application/json"})
+res.writeHead(200,{"Access-Control-Allow-Origin": "*"})
+
+res.end(JSON.stringify(json));
+}
+
+
 
 
     }
@@ -132,7 +168,8 @@ console.log("access!");
 
 });
 
-server.listen(1337, '192.168.0.28', ()=>{
+//TODO stavi internal ip address od servera
+server.listen(1338, 'localhost', ()=>{
 
 console.log("listeningg");
 
